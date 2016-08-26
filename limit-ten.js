d93867025a30090c5b2015cc79bbc1ea01a7cd57
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-08-26 10:05:31 -0500

    limit words search to 10 or less

---
+++ limit-ten.js
@@ -0,0 +1,5 @@
+function limitTen(argsArr) {
+	(argsArr.length) >= 10 ? console.log("Limit Search to 10 words") : console.log("heelo");
+}
+
+module.exports = {limitTen}
--- word-search.js
+++ word-search.js
@@ -2,6 +2,9 @@
 ":" //# comment; exec /usr/bin/env node --harmony "$0" "$@"
 "use strict";
 
+const limit = require('./limit-ten')
+
+
 let [,,...rest] = process.argv
-rest[0] === undefined ? console.log("Usage: word-search req1 [args <= 10]") : console.log("hello");
+rest[0] === undefined ? console.log("Usage: word-search req1 [args <= 10]") : limit.limitTen(rest);
 
